177d5e9d1170eba23320cc110e7657c59bcd3a1e
Rishabhmk25 <rishabhmk25@gmail.com>
2025-12-16 22:57:06 +0530

    optimized soln

--- 2.cpp
+++ 2.cpp
@@ -32,11 +32,10 @@ int main() {
     cin >> s;
     int a1 = s.find("**");
     int a2 = s.find("><");
-    int a3 = s.find(">*<");
-    int a4 = s.find("*<");
-    int a5 = s.find(">*");
+    int a3 = s.find("*<");
+    int a4 = s.find(">*");
     if (a1 != string::npos || a2 != string::npos || a3 != string::npos ||
-        a4 != string::npos || a5 != string::npos) {
+        a4 != string::npos) {
       cout << -1 << endl;
     } else {
       cout << max_time(s) << endl;
